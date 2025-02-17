a41ddf6fe15877b08c8834400c1eb55703982267
Deniel Ivor Cruz <ivorcruz19@gmail.com>
2025-02-17 23:53:29 +0800

    7.3.0

--- app/(tabs)/(settingsTab)/settings.jsx
+++ app/(tabs)/(settingsTab)/settings.jsx
@@ -204,7 +204,7 @@ const settings = () => {
         <SettingsControl title={'Legal & Policy'} icon={'ri-shake-hands-line'}/>
         <SettingsControl title={'Contact Us'} icon={'ri-phone-fill'}/>
         <SettingsControl title={'Log Out'} icon={'ri-logout-circle-line'} onPress={() => setShowLogout(true)}/>
-        <Text className='font-pRegular text-gray-400 text-[10px] text-center'>v7.2.0</Text>
+        <Text className='font-pRegular text-gray-400 text-[10px] text-center'>v7.3.0</Text>
 
         {/* <SettingsControl title={'Delete User'} onPress={toggleDeleteUser}/> */}
         
